fbc6e156454bef9482c7fc7ecf72d13cd0f3223d
sreedhar <sreedhar70755@gmail.com>
2023-08-17 16:17:39 +0530

    tf

--- modules/main.tf
+++ modules/main.tf
@@ -26,7 +26,7 @@ resource "aws_route_table" "main" {
 resource "aws_route_table_association" "aws" {
   for_each = var.subnets
   subnet_id      = lookup(lookup(aws_subnet,each.key, null),id,null)
-  route_table_id = aws_route_table.main[each.key].id
+  route_table_id = aws_route_table.main[each.key].id #bothe do same work
 }
 
 variable "vpc" {}
